76efe11a25b4a230342d625d98d2180c294ca9ff
KarimMartin <karimmartin26@gmail.com>
2024-06-10 13:57:47 +0100

    Add files via upload

---
+++ Day 1 - Glossary.py
@@ -0,0 +1,59 @@
+# Comment
+# A line of code that doesn't run in the compiler and is only visible to the user
+# Useful for taking notes
+
+# Print
+print("Hello world!")
+# Prints hello world to the console
+
+# Input
+input("What is your name")
+# Allows user to input a value
+
+# Variable
+my_name = "Karim"
+# Allows a name to be assigned to a value or piece of data
+
+# The += operator
+my_number = 12
+my_number += 4
+# Takes the previous value and adds to it
+
+# Integers
+my_number = 12
+# Whole numbers
+
+# Floats
+my_float = 3.14
+# Decimal numbers
+
+# Strings
+my_string = "Hello"
+# A string of characters
+
+# String concatenation
+print("Hello" + "Karim")
+# Becomes "HelloKarim"
+
+# F-strings
+days = 365
+print(f"There are {days} in a year")
+# Allows a variable to be inserted into a string
+
+# Converting data types
+# Can convert one data type to another using float(), int() or str()
+n = 354
+new_n = float(n)
+type(n)
+# n will now show as a float
+
+# Lists
+my_list = [1, 2, 3, 4, 5]
+# Lists allow numerous values to be stored
+print(my_list[1])
+# Prints 2, as lists start at index 0.
+
+# Dictionaries
+my_dict = {"Text 1": "hi", "reply": "hello"}
+# Allows key value pairs to be stored.
+
