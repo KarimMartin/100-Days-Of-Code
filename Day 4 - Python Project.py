a302218b6ea9b8f31980d313308620d13323f569
KarimMartin <karimmartin26@gmail.com>
2024-06-18 15:55:49 +0100

    Update Day 4 - Python Project.py
    Finished the slot machine

--- Day 4 - Python Project.py
+++ Day 4 - Python Project.py
@@ -15,6 +15,31 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):  # Looping through every row
+        symbol = columns[0][line]  # Setting symbol to the first column of the current row
+        for column in columns:  # Loop through each column and check for the symbol
+            symbol_to_check = column[line]  # The symbol to check is equal to column at the current row
+            if symbol != symbol_to_check:  # If symbols aren't the same
+                break  # Break out the loop, check the next line
+        else:
+            winnings += values[symbol] * bet  # If no break, user wins multiplier for the symbol times their bet
+            winning_lines.append(lines + 1)
+
+    return winnings, winning_lines
+
+
+
+
 def get_slot_machine_spin(rows, cols, symbols): #Passing 3 parameters to the function.
     # Populating the symbols to go into the slot machine
     all_symbols = []  # Define our list of symbols
@@ -45,8 +70,17 @@ def get_slot_machine_spin(rows, cols, symbols): #Passing 3 parameters to the fun
 def print_slot_machine(columns):
 
     # Flipping the columns so they actually appear as rows:
-    for row in range(len(columns[0])):
-        pass # Need to finish
+    for row in range(len(columns[0])):  # For every row we have
+        for i, column in enumerate(columns):  # Looping through all the items inside 'columns'. Enumerate gives index as well as the item.
+            if i != len(columns) - 1:  # Checking this isn't the last value in the row
+                print(column[row], end=" | ")  # Print the value at the first row of that column. End tells print statement what to end line with.
+            else:
+                print(column[row], end="")  # Print last value without the pipe
+
+        print()  # Brings the row down to another line
+
+
+
 
 
 
@@ -96,9 +130,7 @@ def get_bet():
     return amount  # Function returns the amount
 
 
-
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -109,6 +141,27 @@ def main():
             break
 
     print(f"You are betting ${bet} on {lines} lines. Total bet: ${total_bet}.")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won {winnings}.")
+    print(f"You won on lines:", *winning_lines)
+
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance: ${balance}")
+        answer = input("Press enter to play (q to quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
+
 
 
 main()
